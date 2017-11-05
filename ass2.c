72fb849369a30b02a2f59cafd42486493bc73d6a
Unknown <kaminskiadam1@gmail.com>
2017-11-05 12:10:53 -0500

    isnofakind

--- ass2.c
+++ ass2.c
@@ -11,8 +11,9 @@
 
 //to do
 
-//is_straight
-//is_straight flush (just add straight at straighr flush
+//is_straight // test
+//is_straight flush //test
+
 //is n_of_akind (hint lab 3)
 //is_two_pair (given lab 3)
 //is_full house -iff 2 pair then shouldnt be difficult
@@ -106,14 +107,14 @@ bool is_flush(int cards[], int array_size)
 int is_straight(int cards[], int array_size)
 {
 	bool is_straight = true;
-	int sorta[5];
+	int sorta[array_size];
 	//make new copy array
-	for(int i = 0; i < 5 ; i++)
+	for(int i = 0; i < array_size ; i++)
 	{
 		sorta[i] = cards[i];
 	}
 	//make card number represent non suited card
-	for(int i = 0; i < 5 ; i++)
+	for(int i = 0; i < array_size ; i++)
 	{
 		sorta[i] = sorta[i] % 13;
 	}
@@ -207,10 +208,78 @@ bool is_straight_flush(int cards[], int array_size)
  */
 bool is_n_of_a_kind(int cards[], int array_size, int number_of_a_kind)
 {
+	printf("number of a kind is %d\n", number_of_a_kind);
 	bool n_of_a_kind = false;
+	int sorta[array_size];
+	for(int i = 0; i < array_size ; i++)
+	{
+		sorta[i] = cards[i];
+	}
+	for(int i = 0; i < array_size ; i++)
+	{
+		sorta[i] = sorta[i] % 13;
+	}
+	int w = 0;
+	while(w == 0)
+	{
+		int for_int = 0;
+		for(int i = 0; i <  (array_size - 1) ;i++)
+		{
+			if( sorta[i] > sorta[ i + 1 ])
+			{
+				for_int = 1;
+			}
+		}
+		if (for_int == 0)
+		{
+			w = 1;
+		}
+		for(int i = 0; i <  (array_size - 1) ;i++)
+		{
+			if( sorta[i] > sorta[ i + 1 ])
+			{
+				int num1 = sorta[i];
+				int num2 = sorta[i+1];
+				sorta[i] = num2;
+				sorta[i + 1] = num1;
+			}
+		}
+	}
+	//printf("\n sorted array  n of a kind ,%d %d %d %d %d \n",sorta[0],sorta[1],sorta[2],sorta[3],sorta[4]);
+	//now actual n of a kind
+	int kind[array_size + 1 - number_of_a_kind];
+	for(int i = 0; i <=  (array_size - number_of_a_kind) ;i++)
+	{
+		int kind2[number_of_a_kind - 1];
+		for(int w = 0; w < (number_of_a_kind - 1); w++)
+		{
+			kind2[w] = 1;
+		}
+		for(int b = 0 ; b < (number_of_a_kind - 1 ) ; b++)
+		{
+			if(sorta[b+i] != sorta[b+i+1])  //not working
+			{
+				kind2[b]=0;
+			}
+		}
 
-	// might be easier after doing is_two_pair in the lab
-
+		//printf("kind2 is after checking if the same %d %d\n", kind2[0],kind2[1]); // change to fit kind2specs
+		kind[i]= 1;
+		for(int e = 0 ; e < number_of_a_kind - 1 ;e++)
+		{
+			if(kind2[e] != 1 )
+			{
+				kind[i] = 0;
+			}
+		}
+	}
+	for(int q = 0 ; q < (array_size  - number_of_a_kind) ; q++)
+	{
+		if( kind[q] == 1)
+		{
+			n_of_a_kind = true;
+		}
+	}
 	return n_of_a_kind;
 }
 
@@ -347,17 +416,17 @@ int main(int argc, const char* argv[])
 	 */
 
 	int tester[5];
-	tester[0]= 0;
-	tester[1]= 1;
-	tester[2]= 2;
-	tester[3]= 3;
-	tester[4]= 4;
+	tester[0]= 4;
+	tester[1]= 0;
+	tester[2]= 0;
+	tester[3]= 0;
+	tester[4]= 0;
 	printf("\n tester array %d %d %d %d %d \n",tester[0],tester[1],tester[2],tester[3],tester[4]);
 	//printf("suite not same? %d\n",get_suite (tester[0])!=get_suite(tester[1])  );
 	//printf("tester is_flush %d\n",is_flush(tester, 5) );
-	printf("tester straight %d\n", is_straight(tester,5 ) );
-	printf("tester straight_flush %d\n", is_straight_flush (tester,5 ) );
-
+	//printf("tester straight %d\n", is_straight(tester,5 ) );
+	//printf("tester straight_flush %d\n", is_straight_flush (tester,5 ) );
+	printf("is 4 of a kind %d\n", is_n_of_a_kind (tester,5,4 ) );
 
 
 	return EXIT_SUCCESS;
